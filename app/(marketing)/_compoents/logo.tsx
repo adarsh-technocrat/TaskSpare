2505aaf15b1a43022a130851d92ef6bf28862e8c
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-01 11:13:47 +0530

    chore: added initial commit to task-spare repo 🔥

---
+++ app/(marketing)/_compoents/logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" height="40" width="40" alt="Logo" className="dark:hidden" />
+      <Image src="/logo-dark.svg" height="40" width="40" alt="Logo" className="hidden dark:block" />
+      <text className={cn("font-semibold text-nowrap", font.className)}>Task Spare</text>
+    </div>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://stirring-bluebird-26.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
