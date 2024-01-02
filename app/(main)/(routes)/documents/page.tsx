a7240c7ad06fbeda5f309b6acbacc8312b5e972b
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-02 11:29:41 +0530

    feat: add new note creation API 🎉

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,11 +4,23 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 function DocumentsPage() {
   const { user } = useUser();
 
-  const onCreate = () => {};
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center">
