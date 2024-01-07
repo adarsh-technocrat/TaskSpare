6fe3eb03e0858e4cb2653945ac7b0983effd870b
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-07 22:49:21 +0530

    chore: added Edge Store Provider

--- components/modals/cover-image-model.tsx
+++ components/modals/cover-image-model.tsx
@@ -9,6 +9,7 @@ import { useCoverImage } from "@/hooks/use-cover-image";
 import { SingleImageDropzone } from "@/components/single-image-dropzone";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 export const CoverImageModal = () => {
   const params = useParams();
@@ -18,14 +19,34 @@ export const CoverImageModal = () => {
   const [file, setFile] = useState<File>();
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  const { edgestore } = useEdgeStore();
+
   const onClose = () => {
     setFile(undefined);
     setIsSubmitting(false);
     coverImage.onClose();
   };
 
-  const onChange = async (file?: File) => {};
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
 
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
       <DialogContent>
