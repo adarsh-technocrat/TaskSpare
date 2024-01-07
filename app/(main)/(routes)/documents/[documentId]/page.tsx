0d769c866ce3065abc7cec7fb2d058cdf459192d
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-07 22:10:51 +0530

    chore: added cover Image to document screen

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Cover } from "@/components/cover";
 import Toolbar from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -35,7 +36,7 @@ function DocumentIdPage({ params }: DocumentIdPageProps) {
 
   return (
     <div className="pb-40">
-      {/* <Cover url={document.coverImage} /> */}
+      <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
         {/* <Editor onChange={onChange} initialContent={document.content} /> */}
