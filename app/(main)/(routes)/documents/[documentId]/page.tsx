5581ede5abaa2647a125b8c7d2bc348c27faab6d
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-07 21:44:18 +0530

    chore: compined all the components to document page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,47 @@
-function DocumentIdPage() {
-  return <div>Document Id</div>;
+"use client";
+
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+function DocumentIdPage({ params }: DocumentIdPageProps) {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
+  if (document === undefined) {
+    return <div>Loading ..</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      {/* <Cover url={document.coverImage} /> */}
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+        {/* <Editor onChange={onChange} initialContent={document.content} /> */}
+      </div>
+    </div>
+  );
 }
 
 export default DocumentIdPage;
