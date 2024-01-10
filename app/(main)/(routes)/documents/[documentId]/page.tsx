37e4319208f6776b05cf85302e9d0173a0d7b7f6
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-10 22:37:48 +0530

    feat: added block note editor to document page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Cover } from "@/components/cover";
+import Editor from "@/components/editor";
 import Toolbar from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
@@ -52,7 +53,7 @@ function DocumentIdPage({ params }: DocumentIdPageProps) {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
-        {/* <Editor onChange={onChange} initialContent={document.content} /> */}
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );
