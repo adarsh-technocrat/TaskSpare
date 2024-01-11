a5e2803a34547c645d379dc631d0be7c24109c4f
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-12 00:27:58 +0530

    feat: added the Custom Block Notes Editor
    Signed-off-by: adarsh-technocrat <adarshkumarsingh865@gmail.com>

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import { BlockNoteEditor, PartialBlock, defaultBlockSpecs } from "@blocknote/core";
+import { BlockNoteView, getDefaultReactSlashMenuItems, useBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import { useEdgeStore } from "@/lib/edgestore";
+import { useAiTooling } from "@/hooks/use-ai-tooling";
+import { ContinueWritingBlock } from "./custom_editor_block";
 
 interface EditorProps {
   onChange: (value: string) => void;
@@ -15,6 +17,7 @@ interface EditorProps {
 const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
+  const { listOfCustomAiToolingBackSlashItems } = useAiTooling();
 
   const handleUpload = async (file: File) => {
     const response = await edgestore.publicFiles.upload({
@@ -24,6 +27,16 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     return response.url;
   };
 
+  const customSlashMenuItemList = [
+    ...getDefaultReactSlashMenuItems(),
+    ...listOfCustomAiToolingBackSlashItems,
+  ];
+
+  const blockSpecs: any = {
+    ...defaultBlockSpecs,
+    continueWriting: ContinueWritingBlock,
+  };
+
   const editor: BlockNoteEditor = useBlockNote({
     editable,
     initialContent: initialContent ? JSON.parse(initialContent) : undefined,
@@ -31,6 +44,8 @@ const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
     uploadFile: handleUpload,
+    slashMenuItems: customSlashMenuItemList,
+    blockSpecs: blockSpecs,
   });
 
   return (
