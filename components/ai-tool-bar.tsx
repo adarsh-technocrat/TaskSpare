8df70988656a328728e4218d1f1fd01d5fdadef5
adarsh-technocrat <adarshkumarsingh865@gmail.com>
2024-01-16 15:47:34 +0530

    chore: minor change

--- components/ai-tool-bar.tsx
+++ components/ai-tool-bar.tsx
@@ -14,28 +14,22 @@ function AiToolBar(block: BlockFromConfig) {
 
   return (
     <div className="flex flex-row  justify-between ">
-      {url ? (
-        <p>Image</p>
-      ) : (
-        <>
-          <div className="flex items-center gap-2 flex-1">
-            <Sparkles />
-            <Input
-              placeholder="Ask AI to write anything...."
-              className="bg-transparent border-none outline-none !ring-0 !ring-offset-0 "
-            />
-          </div>
-          <Button
-            onClick={() => {
-              setUrl("URL");
-            }}
-            className="bg-gradient-to-r from-purple-400 to-indigo-600 text-white font-bold py-2 px-4 rounded-full flex flex-row justify-center gap-2 cursor-pointer"
-          >
-            <Sparkles />
-            Generate Content
-          </Button>
-        </>
-      )}
+      <div className="flex items-center gap-2 flex-1">
+        <Sparkles />
+        <Input
+          placeholder="Ask AI to write anything...."
+          className="bg-transparent border-none outline-none !ring-0 !ring-offset-0 "
+        />
+      </div>
+      <Button
+        onClick={() => {
+          setUrl("URL");
+        }}
+        className="bg-gradient-to-r from-purple-400 to-indigo-600 text-white font-bold py-2 px-4 rounded-full flex flex-row justify-center gap-2 cursor-pointer"
+      >
+        <Sparkles />
+        Generate Content
+      </Button>
     </div>
   );
 }
